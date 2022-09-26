bac6aee6adb1d6ef4be7b2c06726e05a8930877a
Lucas Burney <burnelucasd@gmail.com>
2022-09-26 01:46:53 -0500

    Remove second half of generated sentence. Remove the short plan.

--- random-sentence-generator.js
+++ random-sentence-generator.js
@@ -3,16 +3,14 @@ console.clear()
 const absCommand = ['Never, UNDER ANY CIRCUMSTANCE', 'Always', 'Don\'t fret the risk:', 'Let your family know before you', 'Be wary of police activity when you attempt to'];
 const toDo = ['propel yourself forward with violent hip thrusts', 'shake down elderly women', 'make loud complaints about the coarseness of public toilet paper', 'explain to strangers why you deserve to cut in line', 'vigoursly best your high jump record'];
 const place = ['gas station', 'stranger\'s front lawn', 'theme park', 'crowded restroom', 'remote airport'];
-const creatures = ['24 cowboys', 'a band of squirrels', 'several overly-muscled children', 'your in-laws', 'two kindly old women'];
-const presentAction = ['quickly walk in your direction!', 'are scamming tourists.', 'stare at you, dumbfounded.', 'attempt to ignore you.', 'shake down elderly men.', 'arguing with one another.'];
 
 const randomNum = () => {
-  return Math.floor(Math.random() * 5);
+  return Math.floor(Math.random() * 3);
 };
 
 const randomSentence = () => {
   let x = randomNum();
-  console.log(`${absCommand[randomNum()]} ${toDo[randomNum()]} in a ${place[randomNum()]} while ${creatures[randomNum()]} ${presentAction[randomNum()]}`)
+  console.log(`${absCommand[randomNum()]} ${toDo[randomNum()]} in a ${place[randomNum()]}.`)
 
 };
 
