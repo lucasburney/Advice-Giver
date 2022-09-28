2e9645abdff771242cc76e04770b67d5744b3882
Lucas Burney <burnelucasd@gmail.com>
2022-09-28 02:30:43 -0500

    Add basic HTML. Make random sentences smaller.

--- random-sentence-generator.js
+++ random-sentence-generator.js
@@ -1,5 +1,3 @@
-console.clear()
-
 const absCommand = ['Never, UNDER ANY CIRCUMSTANCE', 'Always', 'Don\'t fret the risk:', 'Let your family know before you', 'Be wary of police activity when you attempt to'];
 const toDo = ['propel yourself forward with violent hip thrusts', 'shake down elderly women', 'make loud complaints about the coarseness of public toilet paper', 'explain to strangers why you deserve to cut in line', 'vigoursly best your high jump record'];
 const place = ['gas station', 'stranger\'s front lawn', 'theme park', 'crowded restroom', 'remote airport'];
